799471ce66fcb3767e1aa59cc267ce9797a576ee
Russell Song Jie Foo <rfoo@c2r7s4.42singapore.sg>
2026-05-21 18:35:14 +0800

    chore: add placeholder files for project

---
+++ include/pipex.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/21 17:37:01 by rfoo              #+#    #+#             */
+/*   Updated: 2026/05/21 17:44:28 by rfoo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <sys/wait.h>
+# include "libft.h"
+
+
+
+#endif

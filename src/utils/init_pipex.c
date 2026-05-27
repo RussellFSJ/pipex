12a220dd3533a6e23d185abad541393eb27d78dc
Russell Song Jie Foo <rfoo@c2r7s4.42singapore.sg>
2026-05-28 00:14:11 +0800

    feat: add init_pipex to handle argvs

---
+++ src/utils/init_pipex.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_pipex.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/27 23:43:26 by rfoo              #+#    #+#             */
+/*   Updated: 2026/05/27 23:50:49 by rfoo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+t_pipex	*init_pipex(char **argv, char **envp)
+{
+	t_pipex	*px;
+
+	px->cmd1 = ft_split(argv[2], ' ');
+	px->cmd2 = ft_split(argv[3], ' ');
+	px->envp = envp;
+	px->in_file = argv[1];
+	px->out_file = argv[4];
+	return (px);
+}
